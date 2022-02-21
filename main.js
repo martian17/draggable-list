37037fc38eca9d6c0645a8615398bb47a02eaece
Yutaro Yoshii <yakumodanshi@gmail.com>
2022-02-21 18:36:53 +0900

    added error reporting

--- main.js
+++ main.js
@@ -16,6 +16,13 @@ let main = async function(){
     let body = new ELEM(document.body);
     //let body = new ELEM(document.body);
     body.add(list);
+    
+    let errholder = body.add("div");
+    window.onerror = function(message, source, lineno, colno, error) {
+        console.log("EEE");
+        errholder.add("div",0,`${message}<br>${source}<br>${lineno}<br>${colno}<br>${error}`,"background-color:#f001;margin:10px;");
+    };
+    //setTimeout(()=>{throw new Error("EEEEEE")},1000);
 };
 
 main();
